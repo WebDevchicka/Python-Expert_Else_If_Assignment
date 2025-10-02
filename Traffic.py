455ac990d4323fa538cb0416e6ae46a85b578930
WebDevchicka <152233621+WebDevchicka@users.noreply.github.com>
2025-10-01 21:39:50 -0500

    Create Traffic.py

---
+++ Traffic.py
@@ -0,0 +1,20 @@
+# Smart Traffic Light System
+# DO NOT use max(), sorted(), or any built-in sorting methods.
+# Use ONLY if-elif-else statements for all logic.
+
+# Use this Input structure
+traffic = {
+    "North": {"density": "Medium", "emergency": False},
+    "South": {"density": "High", "emergency": False},
+    "East": {"density": "High", "emergency": True},
+    "West": {"density": "Low", "emergency": False}
+}
+
+#Create function to change density to numeric values
+
+
+#Create function to decide with traffic direction goes first and create contingecy list if more than 1 direction has an emergency
+
+
+# Call the function
+
